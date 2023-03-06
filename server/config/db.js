39d962bfa81627c02b2ecfa0c6737cc80f95b10a
respatica <respatica@hotmail.com>
2023-03-05 20:45:44 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+//camilo patino  id:301223814
+// COMP229 W2023 MID-TERM STUDENT 
+// Date: 3 March 2023
+// mongodb atlas database created and connected
+
+module.exports = {
+  //mongo page  MongoDB deployment ->
+  "URI": "mongodb+srv://admin:admin@clustercomp11.0cydfaz.mongodb.net/book?retryWrites=true&w=majority"
+ 
+};
